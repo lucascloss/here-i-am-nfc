ecac854d1df10d7659f33fa98742a85e4721b297
lucas.closs <lucas.closs@79e836b9-f228-daa8-080e-26799d1ae1a5>
2014-04-02 03:57:29 +0000

    #16. Keep Mapoverlay

--- trunk/hereiam_and/src/com/hereiam/helper/NFCReader.java
+++ trunk/hereiam_and/src/com/hereiam/helper/NFCReader.java
@@ -25,7 +25,6 @@ public class NFCReader{
          
         Ndef ndef = Ndef.get(tag);
         if (ndef == null) {
-            // NDEF is not supported by this Tag. 
             return null;
         }
  
@@ -48,29 +47,14 @@ public class NFCReader{
     private String readText(NdefRecord record) throws UnsupportedEncodingException {         
         byte[] payload = record.getPayload();
  
-        // Get the Text Encoding
         String textEncoding = ((payload[0] & 128) == 0) ? "UTF-8" : "UTF-16";
  
-        // Get the Language Code
         int languageCodeLength = payload[0] & 0063;
          
-        // String languageCode = new String(payload, 1, languageCodeLength, "US-ASCII");
-        // e.g. "en"
-         
-        // Get the Text
         return new String(payload, languageCodeLength + 1, payload.length - languageCodeLength - 1, textEncoding);
     }
-     
-    /*@Override 	
-    protected void onPostExecute(String result) {
-        if (result != null) {
-            //mTextView.setText("Read content: " + result);
-        	Log.v(TAG, result);
-        }
-    }*/
     
     public static NdefMessage[] getNdefMessages(Intent intent) {
-        // Parse the intent
         NdefMessage[] msgs = null;
         String action = intent.getAction();
         if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(action)
@@ -83,7 +67,6 @@ public class NFCReader{
                     msgs[i] = (NdefMessage) rawMsgs[i];
                 }
             } else {
-                // Unknown tag type
                 byte[] empty = new byte[] {};
                 NdefRecord record = 
                     new NdefRecord(NdefRecord.TNF_UNKNOWN, empty, empty, empty);
