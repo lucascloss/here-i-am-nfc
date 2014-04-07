0ef1208499782195a34c4f123be6ef30ee89cd26
lucas.closs <lucas.closs@79e836b9-f228-daa8-080e-26799d1ae1a5>
2014-04-07 20:15:00 +0000

    - Tela ManterAmbiente/modal - Tela ManterLocal/modal

--- hereiam_manager/public/js/ManterAmbiente.js
+++ hereiam_manager/public/js/ManterAmbiente.js
@@ -1,3 +1,15 @@
-/**
- * 
- */
+$(document).ready(function() {
+    $('#listaAmbientes').dataTable();
+    
+    $('#btnAdicionar').click(function() { 
+//    	$(location).attr('href', 'index.php?controle=manterambiente&acao=adicionar');
+    });
+    
+    $('#btnEditar').click(function() { 
+    	$(location).attr('href', 'index.php?controle=manterambiente&acao=editar');
+    });
+    
+    $('#btnRemover').click(function() { 
+    	$(location).attr('href', 'index.php?controle=manterambiente&acao=remover');
+    });
+} );
--- hereiam_manager/public/js/ManterLocal.js
+++ hereiam_manager/public/js/ManterLocal.js
@@ -1,3 +1,15 @@
-/**
- * 
- */
+$(document).ready(function() {
+    $('#listaLocais').dataTable();
+    
+    $('#btnAdicionar').click(function() { 
+//    	$(location).attr('href', 'index.php?controle=manterlocal&acao=adicionar');
+    });
+    
+    $('#btnEditar').click(function() { 
+    	$(location).attr('href', 'index.php?controle=manterlocal&acao=editar');
+    });
+    
+    $('#btnRemover').click(function() { 
+    	$(location).attr('href', 'index.php?controle=manterlocal&acao=remover');
+    });
+} );
