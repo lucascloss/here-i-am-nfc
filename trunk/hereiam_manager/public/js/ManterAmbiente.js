3772fbd6212eaaeb608a294b5d82e1f087ce7eb2
lucas.closs <lucas.closs@79e836b9-f228-daa8-080e-26799d1ae1a5>
2014-04-09 02:49:41 +0000

    - Janelas Modal

--- trunk/hereiam_manager/public/js/ManterAmbiente.js
+++ trunk/hereiam_manager/public/js/ManterAmbiente.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $('#listaAmbientes').dataTable();
     
     $('#btnAdicionar').click(function() { 
-//    	$(location).attr('href', 'index.php?controle=manterambiente&acao=adicionar');
+    	$(location).attr('href', 'index.php?controle=manterambiente&acao=adicionar');
     });
     
     $('#btnEditar').click(function() { 
@@ -12,4 +12,17 @@ $(document).ready(function() {
     $('#btnRemover').click(function() { 
     	$(location).attr('href', 'index.php?controle=manterambiente&acao=remover');
     });
+//    aqui
+    $('[data-toggle="modal"]').click(function(e) {
+    	e.preventDefault();
+    	var url = $(this).attr('href');
+    	if (url.indexOf('#') == 0) {
+    		$(url).modal('open');
+    	} else {
+    		$.get(url, function(data) {
+    			$('<div class="modal hide fade">' + data + '</div>').modal();
+    		}).success(function() { $('input:text:visible:first').focus(); });
+    	}
+    });
+//    aqui
 } );
