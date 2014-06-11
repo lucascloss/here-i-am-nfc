af38d7dea1c99f9cd676e96351971914d7debffb
email@guiwunsch.com <email@guiwunsch.com@79e836b9-f228-daa8-080e-26799d1ae1a5>
2014-06-11 04:26:53 +0000

    - Ajustes finais na tela de locais.

--- hereiam_manager/public/js/ManterLocal.js
+++ hereiam_manager/public/js/ManterLocal.js
@@ -13,9 +13,6 @@ $(document).ready(function() {
         ]        
     } );
     
-    $("#btnEditarLocal").prop( "disabled", true );
-    $("#btnRemoverLocal").prop( "disabled", true );
-    
     $('#listaLocais tbody').on( 'click', 'tr', function () {
         $(this).toggleClass('selected');
         
@@ -29,82 +26,92 @@ $(document).ready(function() {
     });
         
     $('#modalAdicionar').on('show.bs.modal', function (e) {
+    	$('.progressBar').show();
  		
  		$.ajax({
          	type: "GET",
          	url: URL_BASE + "/environments",        	
          	success: function(data) {
-         		$('#environmentId option').remove();
+         		$('#adicionarLocal #environmentId option').remove();
          		for (var i = 0; i < data.length; i++) {         			
-         			$('#environmentId').append($("<option></option>").text(data[i].name));
+         			$('#adicionarLocal #environmentId').append($("<option></option>").text(data[i].name).val(data[i].id));
          		}
              } , 
-     	dataType: "json"
+             dataType: "json"
         });
 
  	    $.ajax({
          	type: "GET",
          	url: URL_BASE + "/placeAdms",        	
          	success: function(data) {
-         		$('#placeAdmId option').remove();         		
+         		$('#adicionarLocal #placeAdmId option').remove();         		
          		for (var i = 0; i < data.length; i++) {         			
-         			$('#placeAdmId').append($("<option></option>").text(data[i].name));
-         		}        	           		        	
+         			$('#adicionarLocal #placeAdmId').append($("<option></option>").text(data[i].name).val(data[i].id));
+         		}
+         		
+         		$('.progressBar').hide();
              } , 
-     	dataType: "json"
-        });         		         		
+             dataType: "json"
+        });   
     });
       
     $('#modalEditar').on('loaded.bs.modal', function (e) {    	    	    	    
-	    $(this).find("form").submit($.editarLocal);	   	   
+	    $(this).find("form").submit($.editarLocal);
+	    
+	    $('#editarLocal #environmentId').prop( 'disabled', true );
+	    $('#editarLocal #placeAdmId').prop( 'disabled', true );
     });
     
     $('#modalEditar').on('show.bs.modal', function (e) {
     	var selected = $("#listaLocais").DataTable().rows('.selected').data();
     	
-    	//iniciar progressbar ?
+    	$('.progressBar').show();
 	    
 	    $.ajax({
         	type: "GET",
         	url: WS_URL + "/" + selected[0].id,        	
         	success: function(data) {
-        		$("#name").val(data.name);
-        		$("#type").val(data.type);
-        		$("#info").val(data.info);
-        		$("#idNfc").val(data.idNfc);
-        		$("#latitude").val(data.latitude);
-        		$("#longitude").val(data.longitude);
+        		$("#editarLocal #name").val(data.name);
+        		$("#editarLocal #type").val(data.type);
+        		$("#editarLocal #info").val(data.info);
+        		$("#editarLocal #idNfc").val(data.idNfc);
+        		$("#editarLocal #latitude").val(data.latitude);
+        		$("#editarLocal #longitude").val(data.longitude);
         		
-        		$.ajax({
-                	type: "GET",
-                	url: URL_BASE + "/environments/" + data.environmentId,        	
-                	success: function(data1) {                		
-                		$('#environmentId option').remove();
-                		$('#environmentId').append($("<option></option>").text(data1.name));                		        	           		        	
-                    } , 
-            	dataType: "json"
-                });
-
-        	    $.ajax({
-                	type: "GET",
-                	url: URL_BASE + "/placeAdms/" + data.placeAdmId,        	
-                	success: function(data2) {
-                		$('#placeAdmId option').remove();
-                		$('#placeAdmId').append($("<option></option>").text(data2.name));        	           		        	
-                    } , 
-            	dataType: "json"
-                });
+        		if ((data.environmentId != null) && (data.environmentId > 0)) {
+	        		$.ajax({
+	                	type: "GET",
+	                	url: URL_BASE + "/environments/" + data.environmentId,        	
+	                	success: function(data1) {                		
+	                		$('#editarLocal #environmentId option').remove();
+	                		$('#editarLocal #environmentId').append($("<option></option>").text(data1.name).attr( "selected", "selected" ));                		        	           		        	
+	                    } , 
+	                    dataType: "json"
+	                });
+        		}
+        		
+        		if ((data.placeAdmId != null) && (data.placeAdmId > 0)) {
+	        	    $.ajax({
+	                	type: "GET",
+	                	url: URL_BASE + "/placeAdms/" + data.placeAdmId,        	
+	                	success: function(data2) {
+	                		$('#editarLocal #placeAdmId option').remove();
+	                		$('#editarLocal #placeAdmId').append($("<option></option>").text(data2.name).attr( "selected", "selected" ));        	           		        	
+	                    } , 
+	                    dataType: "json"
+	                });
+        		}
         		
         		if(data.important == true){
-        			$(':checkbox').prop('checked', true);
-        		}else {
-        			$(':checkbox').prop('checked', false);
-        		}        		        		
-            } , 
-    	dataType: "json"
+        			$('#editarLocal #important').prop('checked', true);
+        		} else {
+        			$('#editarLocal #important').prop('checked', false);
+        		}
+        		
+        		$('.progressBar').hide();
+            }, 
+            dataType: "json"
         });
-	    
-	    //finalizar progressbar ?
     });
     
     $('#modalRemover').on('loaded.bs.modal', function (e) {
@@ -112,22 +119,26 @@ $(document).ready(function() {
     });
     
     $.adicionarLocal = function (event) {
+    	$('.progressBar').show();
+    	
     	var obj = new Object();
-        obj.name = $("#name").val();
-        obj.type  = $("#type").val();
-        obj.info = $("#info").val();
-        obj.idNfc = $("#idNfc").val();
-        obj.latitude = $("#latitude").val();
-        obj.longitude = $("#longitude").val();
-        obj.environmentId = $("#environmentId").val();
-        obj.placeAdmId = $("#placeAdmId").val();
-        obj.important = $("#important").val();
+        obj.name = $("#adicionarLocal #name").val();
+        obj.type  = $("#adicionarLocal #type").val();
+        obj.info = $("#adicionarLocal #info").val();
+        obj.idNfc = $("#adicionarLocal #idNfc").val();
+        obj.latitude = $("#adicionarLocal #latitude").val();
+        obj.longitude = $("#adicionarLocal #longitude").val();
+        obj.environmentId = $("#adicionarLocal #environmentId").val();
+        obj.placeAdmId = $("#adicionarLocal #placeAdmId").val();
+        obj.important = $("#adicionarLocal #important").val();
         
         $.ajax({
         	type: "POST",
         	url: WS_URL,
         	data: obj,
         	success: function(data) {
+        		$('.progressBar').hide();
+        		
         		$("#modalAdicionar").modal("hide");
         		$("#listaLocais").dataTable()._fnAjaxUpdate();
             }
@@ -137,27 +148,30 @@ $(document).ready(function() {
     };
     
     $.editarLocal = function (event) {
+    	$('.progressBar').show();
+    	
     	var selected = $("#listaLocais").DataTable().rows('.selected').data();
     	
     	if (selected.length == 1) {
 	    	var obj = new Object();
-	        obj.name = $("#name").val();
-	        obj.type  = $("#type").val();
-	        obj.info = $("#info").val();
-	        obj.idNfc = $("#idNfc").val();
-	        obj.latitude = $("#latitude").val();
-	        obj.longitude = $("#longitude").val();
-	        obj.environmentId = $("#environmentId").val();
-	        obj.placeAdmId = $("#placeAdmId").val();
-	        obj.important = $("#important").val();
+	        obj.name = $("#editarLocal #name").val();
+	        obj.type  = $("#editarLocal #type").val();
+	        obj.info = $("#editarLocal #info").val();
+	        obj.idNfc = $("#editarLocal #idNfc").val();
+	        obj.latitude = $("#editarLocal #latitude").val();
+	        obj.longitude = $("#editarLocal #longitude").val();
+	        //obj.environmentId = $("#editarLocal #environmentId").val();
+	        //obj.placeAdmId = $("#editarLocal #placeAdmId").val();
+	        obj.important = $("#editarLocal #important").val();
 	        
 	        $.ajax({
 	        	type: "PUT",
 	        	url: WS_URL + "/" + selected[0].id,
 	        	data: obj,
 	        	success: function(data) {
+	        		$('.progressBar').hide();
 	        		$("#modalEditar").modal("hide");
-	        		$("#listaLocais").dataTable()._fnAjaxUpdate();
+	        		$.reload();
 	            }, 
 	        	dataType: "json"
 	        });
@@ -167,6 +181,7 @@ $(document).ready(function() {
     };
     
     $.removerLocal = function (event) {
+    	$('.progressBar').show();
     	var selected = $("#listaLocais").DataTable().rows('.selected').data();
 
     	if (selected.length > 0) {
@@ -175,8 +190,9 @@ $(document).ready(function() {
 		        	type: "DELETE",
 		        	url: WS_URL + "/" + selected[i].id,
 		        	success: function(data) {
+		        		$('.progressBar').hide();
 		        		$("#modalRemover").modal("hide");
-		        		$("#listaLocais").dataTable()._fnAjaxUpdate();
+		        		$.reload();
 		            }, 
 		        	dataType: "json"
 		        });
@@ -184,6 +200,18 @@ $(document).ready(function() {
     	}
 
     	event.preventDefault();
+    };
+    
+    $init = function() {
+    	$("#btnEditarLocal").prop( "disabled", true );
+        $("#btnRemoverLocal").prop( "disabled", true );
+    };
+    $init();
+    
+    $.reload = function() {
+        $.init();
+        
+        $("#listaLocais").dataTable()._fnAjaxUpdate();
     };
     
 });
