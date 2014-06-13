6b203db5a87492b9d56f1ec5b7c4a3630d75bb6e
lucas.closs <lucas.closs@79e836b9-f228-daa8-080e-26799d1ae1a5>
2014-06-13 02:43:17 +0000

    Manter ambiente, local e usuario. Inicio manter agenda.

--- hereiam_manager/public/js/ManterUsuario.js
+++ hereiam_manager/public/js/ManterUsuario.js
@@ -1,25 +1,59 @@
 $(document).ready(function() {
-	
-    $('#listaUsuarios').dataTable( {
-        "ajax": URL_BASE + "/appUsers",
+	WS_URL = URL_BASE + "/appUsers";
+    
+	$('.progressBar').show();
+	$('#listaUsuarios').DataTable( {
+        "ajax": WS_URL,
         "ajaxDataProp": "",
         "columns": [
-            //{ "data": "id" },
+            { "data": "id" },
             { "data": "name" },
             { "data": "email" },
             { "data": "username" }	            
         ]
     } );
     
-    /*$('#btnAdicionar').click(function() { 
-    	$(location).attr('href', 'index.php?controle=manterlocal&acao=adicionar');
-    });
-    
-    $('#btnEditar').click(function() { 
-    	$(location).attr('href', 'index.php?controle=manterlocal&acao=editar');
-    });
-    
-    $('#btnRemover').click(function() { 
-    	$(location).attr('href', 'index.php?controle=manterlocal&acao=remover');
-    });*/
+	 $('#listaUsuarios tbody').on( 'click', 'tr', function () {
+	        $(this).toggleClass('selected');
+	        
+	        var selected = $("#listaUsuarios").DataTable().rows('.selected').data();	        
+	        $("#btnRemoverUsuario").attr( "disabled", (selected.length == 0) );
+	  } );
+	 
+	 $('#modalRemover').on('loaded.bs.modal', function (e) {
+	    	$(this).find("form").submit($.removerUsuario);
+	 });
+	 
+	 $.removerUsuario = function (event) {
+	    	$('.progressBar').show();
+	    	var selected = $("#listaUsuarios").DataTable().rows('.selected').data();
+
+	    	if (selected.length > 0) {
+	    		for (var i = 0; i < selected.length; i++) {
+			        $.ajax({
+			        	type: "DELETE",
+			        	url: WS_URL + "/" + selected[i].id,
+			        	success: function(data) {
+			        		$('.progressBar').hide();
+			        		$("#modalRemover").modal("hide");
+			        		$.reload();
+			        		$.init();
+			            }, 
+			        	dataType: "json"
+			        });
+	    		}
+	    	}
+
+	    	event.preventDefault();
+	    };
+	    
+	    $init = function() {	    	
+	        $("#btnRemoverUsuario").attr( "disabled", true );
+	    };
+	    $init();
+	    
+	    $.reload = function() {
+//	        $.init();
+	        $("#listaUsuarios").dataTable()._fnAjaxUpdate();
+	    };
 } );
