c8d0040b2aad3334fdab19bdc323179f91abfe4f
email@guiwunsch.com <email@guiwunsch.com@79e836b9-f228-daa8-080e-26799d1ae1a5>
2014-06-14 04:12:31 +0000

    - Finalizado Manter Administrador.

--- trunk/hereiam_manager/public/js/ManterAdministrador.js
+++ trunk/hereiam_manager/public/js/ManterAdministrador.js
@@ -1,52 +1,167 @@
 $(document).ready(function() {
 	
     if ($('#listaAdministradores-sistema').length > 0) {
-    	alert('passou aqui'); 
-	    $('#listaAdministradores-sistema').dataTable( {
-	        "ajax": URL_BASE + "/solutionAdms",
+    	WS_URL = URL_BASE + "/solutionAdms";
+	    var dTable = $('#listaAdministradores-sistema').DataTable( {
+	        "ajax": WS_URL,
 	        "ajaxDataProp": "",
 	        "columns": [
-	                    //{ "data": "id" },
+	                    { "data": "id" },
 	                    { "data": "name" },
 	                    { "data": "email" },
 	                    { "data": "username" }	  
 	        ]
 	    } );
     } else if ($('#listaAdministradores-ambiente').length > 0) {
-	    $('#listaAdministradores-ambiente').dataTable( {
-	        "ajax": URL_BASE + "/environmentAdms",
+    	WS_URL = URL_BASE + "/environmentAdms";
+    	var dTable = $('#listaAdministradores-ambiente').DataTable( {
+	        "ajax": WS_URL,
 	        "ajaxDataProp": "",
 	        "columns": [
-	                    //{ "data": "id" },
+	                    { "data": "id" },
 	                    { "data": "name" },
 	                    { "data": "email" },
 	                    { "data": "username" }	  
 	        ]
 	    } );
     } else if ($('#listaAdministradores-local').length > 0) {
-	    $('#listaAdministradores-local').dataTable( {
-	        "ajax": URL_BASE + "/placeAdms",
+    	WS_URL = URL_BASE + "/placeAdms";
+    	var dTable = $('#listaAdministradores-local').DataTable( {
+	        "ajax": WS_URL,
 	        "ajaxDataProp": "",
 	        "columns": [
-	                    //{ "data": "id" },
+	                    { "data": "id" },
 	                    { "data": "name" },
 	                    { "data": "email" },
 	                    { "data": "username" }	  
 	        ]
 	    } );
     }
+
+    $("[id^=listaAdministradores-] tbody").on( 'click', 'tr', function () {
+        $(this).toggleClass('selected');
+        
+        var selected = dTable.rows('.selected').data();
+        $("#btnEditarAdministrador").attr( "disabled", (selected.length != 1) );
+        $("#btnRemoverAdministrador").attr( "disabled", (selected.length == 0) );
+    } );
     
-    $('#btnAdicionar').click(function() { 
-    	$(location).attr('href', 'index.php?controle=manteradministrador&acao=adicionar');
+    $('#modalAdicionar').on('loaded.bs.modal', function (e) {
+    	$(this).find("form").submit($.adicionarAdministrador);
+    });   
+    
+    $('#modalEditar').on('loaded.bs.modal', function (e) {    	    	    	    
+	    $(this).find("form").submit($.editarAdministrador);
+	    
+	    $('#editarLocal #username').attr( 'disabled', true );
     });
     
-    $('#btnEditar').click(function() { 
-    	$(location).attr('href', 'index.php?controle=manteradministrador&acao=editar');
+    $('#modalEditar').on('show.bs.modal', function (e) {
+    	var selected = dTable.rows('.selected').data();
+    	
+    	$('.progressBar').show();
+	    
+	    $.ajax({
+        	type: "GET",
+        	url: WS_URL + "/" + selected[0].id,        	
+        	success: function(data) {
+        		$("#editarAdministrador #name").val(data.name);
+        		$("#editarAdministrador #username").val(data.username);
+        		$("#editarAdministrador #email").val(data.email);
+        		$("#editarAdministrador #password").val(data.password);
+        		
+        		$('.progressBar').hide();
+            }, 
+            dataType: "json"
+        });
     });
     
-    $('#btnRemover').click(function() { 
-    	$(location).attr('href', 'index.php?controle=manteradministrador&acao=remover');
+    $('#modalRemover').on('loaded.bs.modal', function (e) {
+    	$(this).find("form").submit($.removerAdministrador);
     });
     
-    $('#teste').datepicker();
+    $.adicionarAdministrador = function (event) {
+    	$('.progressBar').show();
+    	
+    	var obj = new Object();
+        obj.name = $("#adicionarAdministrador #name").val();
+        obj.username  = $("#adicionarAdministrador #username").val();
+        obj.email = $("#adicionarAdministrador #email").val();
+        obj.password = $("#adicionarAdministrador #password").val();
+        
+        $.ajax({
+        	type: "POST",
+        	url: WS_URL,
+        	data: obj,
+        	success: function(data) {
+        		$('.progressBar').hide();
+        		$("#modalAdicionar").modal("hide");
+        		$.reload();
+            }
+        });
+        
+        event.preventDefault();
+    }; 
+    
+    $.editarAdministrador = function (event) {
+    	$('.progressBar').show();
+    	
+    	var selected = dTable.rows('.selected').data();
+
+    	if (selected.length == 1) {
+	    	var obj = new Object();
+	        obj.name = $("#editarAdministrador #name").val();
+	        obj.username  = $("#editarAdministrador #username").val();
+	        obj.email = $("#editarAdministrador #email").val();
+	        obj.password = $("#editarAdministrador #password").val();
+	        
+	        console.log(obj);
+	        $.ajax({
+	        	type: "PUT",
+	        	url: WS_URL + "/" + selected[0].id,
+	        	data: obj,
+	        	success: function(data) {
+	        		$('.progressBar').hide();
+	        		$("#modalEditar").modal("hide");
+	        		$.reload();
+	            }, 
+	        	dataType: "json"
+	        });
+    	}
+        
+    	event.preventDefault();
+    };
+    
+    $.removerAdministrador = function (event) {
+    	$('.progressBar').show();
+    	var selected = dTable.rows('.selected').data();
+
+    	if (selected.length > 0) {
+    		for (var i = 0; i < selected.length; i++) {
+		        $.ajax({
+		        	type: "DELETE",
+		        	url: WS_URL + "/" + selected[i].id,
+		        	success: function(data) {
+		        		$('.progressBar').hide();
+		        		$("#modalRemover").modal("hide");
+		        		$.reload();
+		            }, 
+		        	dataType: "json"
+		        });
+    		}
+    	}
+
+    	event.preventDefault();
+    };
+    
+    $.init = function() {
+    	$("#btnEditarAdministrador").attr( "disabled", true );
+        $("#btnRemoverAdministrador").attr( "disabled", true );
+    };
+    $.init();
+    
+    $.reload = function() {
+    	$.init();
+        dTable.ajax.reload();
+    };
 } );
